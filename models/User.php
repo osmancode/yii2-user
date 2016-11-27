bbc0197481f3d07bdd7452d98855763774080023
Mohammed Othman <osmancode@gmail.com>
2016-11-27 22:01:10 +0100

    Update User.php

--- models/User.php
+++ models/User.php
@@ -518,7 +518,7 @@ class User extends ActiveRecord implements IdentityInterface
     /** @inheritdoc */
     public static function tableName()
     {
-        return '{{%user}}';
+        return self::getModule()->table_user;
     }
 
     /** @inheritdoc */
